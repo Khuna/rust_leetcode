0645d92365a1e5872dd7aec566022de5377827b7
khuna <akhund.shemsedinov@gmail.com>
2025-05-01 18:04:04 +0300

    solved two_sum problem

--- src/problems/two_sum.rs
+++ src/problems/two_sum.rs
@@ -1,8 +1,8 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map = HashMap::with_capacity(nums.len());
+        let mut map: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
         for (i, num) in nums.iter().enumerate() {
             match map.get(&num) {
                 Some(&j) => return vec![i as i32, j as i32],
