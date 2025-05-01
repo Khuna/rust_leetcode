f4cd8ec21f6b91586079ef9f114962e31de8757f
khuna <akhund.shemsedinov@gmail.com>
2025-05-01 17:49:38 +0300

    1st commit

---
+++ src/problems/two_sum.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::with_capacity(nums.len());
+        for (i, num) in nums.iter().enumerate() {
+            match map.get(&num) {
+                Some(&j) => return vec![i as i32, j as i32],
+                None => {
+                    map.insert(target - num, i);
+                }
+            }
+        }
+        unreachable!();
+    }
+}
