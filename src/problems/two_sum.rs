81ed096875bd9da5fc4469ed6a2c17a39c0fe6cb
khuna <akhund.shemsedinov@gmail.com>
2025-05-04 00:43:49 +0300

    mmm

--- src/problems/two_sum.rs
+++ src/problems/two_sum.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap};
+use std::collections::HashMap;
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut map: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
